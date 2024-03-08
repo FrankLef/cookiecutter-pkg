4790108b9c1331402ed62cbe3b2b32e750ba3852
FrankLef <flefebvre01@hotmail.com>
2024-03-08 13:17:12 -0500

    feat: main

--- {{cookiecutter.__package_slug}}/src/{{cookiecutter.__package_slug}}/main.py
+++ {{cookiecutter.__package_slug}}/src/{{cookiecutter.__package_slug}}/main.py
@@ -1,1 +1,21 @@
 """Main code file"""
+
+from rich import print
+
+
+def say_hello(name: str = "Ephel") -> str:
+    """Example function to say hello. To be deleted.
+
+    Args:
+        name (str, optional): Name to use to say hello. Defaults to "Ephel".
+
+    Returns:
+        str: Hello message.
+    """
+    msg = "[cyan]" + " ".join(["\u2139", "Hello", name]) + "[/cyan]"
+    print(msg)
+    return msg
+
+
+if __name__ == "__main__":
+    say_hello()
