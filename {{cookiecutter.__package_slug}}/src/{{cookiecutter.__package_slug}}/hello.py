b872a5663fea5804e2663d3da61a95767088896d
FrankLef <flefebvre01@hotmail.com>
2025-11-11 04:44:35 -0500

    feat(hello)

--- {{cookiecutter.__package_slug}}/src/{{cookiecutter.__package_slug}}/hello.py
+++ {{cookiecutter.__package_slug}}/src/{{cookiecutter.__package_slug}}/hello.py
@@ -6,6 +6,8 @@ from rich import print
 
 def hello(name: str = getlogin()) -> str:
     """Example function to say hello. To be deleted.
+    
+    This function can be run from the cmd prompt with 'py -m fltk'.
 
     Args:
         name (str, optional): Name to use to say hello. Default is current user.
@@ -13,12 +15,12 @@ def hello(name: str = getlogin()) -> str:
     Returns:
         str: Hello message.
     """
-    time_stamp: datetime = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
-    msg: str = " ".join(["\u2139", "Hello", name, time_stamp])
+    time_stamp: str = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+    msg: str = " ".join(["\u2139", " Hello", name, time_stamp])
     msg = "[cyan]" + msg + "[/cyan]"
     print(msg)
     return msg
 
 
-if __name__ == "__main__":
-    hello()
+# if __name__ == "__main__":
+#     hello()
